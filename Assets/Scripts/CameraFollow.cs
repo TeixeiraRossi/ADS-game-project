b1050986bdca4e8c7c2e7095d620603664ff8ac0
Guilherme <alukrd02@hotmail.com>
2026-08-18 12:32:43 -0300

    camera funcionando se pa

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static Vector2 LimitsY = new Vector2(-0.594f, -1.636f);
+
+    [SerializeField]
+    public Vector2 limitsY = new Vector2(-0.594f, -1.636f);
+
+    public float offset;
+    public Transform target;
+
     void Start()
     {
-        
+        LimitsY = limitsY;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!target)
+            return;
+        transform.position = new Vector3(offset + target.position.x,
+            transform.position.y,
+            transform.position.z);
     }
 }
